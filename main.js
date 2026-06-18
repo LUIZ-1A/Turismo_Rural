ffbfff7b702d31e68b3419f818f6a0dbc38bac0b
LUIZ-1A <167090144+LUIZ-1A@users.noreply.github.com>
2026-06-18 09:43:29 -0300

    s

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-function mostrarMensagem() {
-  alert("Ótima escolha! Em breve você viverá uma experiência no turismo rural 🌿");
+function scrollDestinos() {
+  document.getElementById("destinos").scrollIntoView({ behavior: "smooth" });
 }
+
+function abrirModal(titulo, texto) {
+  document.getElementById("modal-title").innerText = titulo;
+  document.getElementById("modal-text").innerText = texto;
+  document.getElementById("modal").style.display = "block";
+}
+
+function fecharModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+window.onclick = function(event) {
+  const modal = document.getElementById("modal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
